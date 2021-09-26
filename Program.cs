248fa185b7ecfa3520271c3dc899a1f692ab1c60
gabrielhenrique123 <gabycristoffano@gmail.com>
2021-09-25 21:03:13 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int equaçao, contador, numero;
+
+           
+            Console.Write("INSIRA UM NUMERO PARA OBTER UMA TABUADA  : ");
+            numero = Int32.Parse(Console.ReadLine());
+
+            
+            for (contador = 1; contador <= 10; ++contador)
+            {
+                equaçao =  numero * contador;
+                Console.WriteLine(numero + "  x  " + contador + "  =  " + equaçao);
+                
+            }
+
+            
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Comandos do git
+1-git init - (comando para inicializar o git)
+2- git remote add origin https://github.com/gabrielhenrique123/My-Projets.git
+3- git remote -v (usado para vizualizar o repositorio atual )
+4- git commit -n "nome do comit"
